bd97379c510b87a48db086ec83e6e47b3128f1a3
Steven Aubertin <stevenaubertin@gmail.com>
2018-12-21 22:05:29 -0500

    finished day2 part 1

--- 2018/Day2/Program.fs
+++ 2018/Day2/Program.fs
@@ -5,20 +5,27 @@ let exactlyNLetter n str =
     |> Seq.countBy (fun x -> x)
     |> Seq.map (fun grp -> snd grp)
     |> Seq.filter (fun count -> count = n)
-    |> Seq.length
-    |> Seq.a
+    |> Seq.length >= 1
 
 let exactly2Letter str = exactlyNLetter 2 str
 let exactly3Letter str = exactlyNLetter 3 str
 
+let part1 filename = 
+    let products = 
+        filename
+        |> Files.readLines
+        |> Seq.map (fun line -> (exactly2Letter line, exactly3Letter line))
+        |> Seq.fold (fun acc next -> match next with
+                                     | (true, true) -> ((fst acc) + 1, (snd acc) + 1)
+                                     | (true, false) -> ((fst acc) + 1, (snd acc))
+                                     | (false, true) -> ((fst acc), (snd acc) + 1)
+                                     | (false, false) -> acc) (0, 0)
+    (fst products) * (snd products)
+
 [<EntryPoint>]
 let main argv =
-    //Seq.last argv
-    @"C:\Users\saubertin\repo\adventofcode\2018\Day2\Test.txt"
-    |> Files.readLines
-    |> Seq.map (fun line -> (exactly2Letter line, exactly3Letter line))
-    |> Seq.iter (fun x -> printfn "%A" x)
-    //|> Seq.reduce (fun acc next -> ((fst acc) + (fst next), (snd acc) + (snd acc)))
-    //|> printfn "%A"
+    Seq.last argv
+    |> part1
+    |> printf "%i\r\n"
 
     0 // return an integer exit code
